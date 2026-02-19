cdaf1af666cf8b294f31a9a868191dced17aa515
Rai <felicianohenry760@gmail.com>
2026-02-19 12:36:46 +0800

    Version 5

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 console.log('Hello, World!');
 console.log('This is a JavaScript file.');
-console.log('this is a version4.0');
+console.log('this is a version5.0');
 
-console.log('This is a new line added in version 4.0');
+console.log('This is a new line added in version 5.0');
